26eaf017e4d960a22c2bd28bdcf46d3c5a15f312
vkrpk <victor.krupka@orange.fr>
2024-06-26 10:16:28 +0200

    add dynamic ListView, include assets, and implement conditional display logic with image associations

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,16 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -61,37 +35,41 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
       _fruits.add(_counter);
     });
   }
 
+  getColor(number) {
+    if (number % 2 == 0) {
+      return Colors.indigo;
+    } else {
+      return Colors.cyan;
+    }
+  }
+
+  bool isPrime(int num) {
+    if (num <= 1) {
+      return false;
+    }
+
+    for (int i = 2; i <= num / 2; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -105,24 +83,27 @@ class _MyHomePageState extends State<MyHomePage> {
             ListView.builder(
               itemCount: _fruits.length,
               shrinkWrap: true,
+              // prototypeItem: ListTile(
+              //   title: Text(_fruits.first.toString(), style: const TextStyle(color: Colors.red)),
+              // ),
               itemBuilder: (BuildContext context, int index) {
                 return ListTile(
-                  title: Text(_fruits[index].toString()),
+                  title: Text(_fruits[index].toString(), style: const TextStyle(
+                    color: Colors.white,
+                  )),
+                  tileColor: getColor(index),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(isPrime(index + 1) ? 'images/ananas.png' : getColor(index + 1) == Colors.indigo ? 'images/poire.png' : 'images/pomme.png'),
+                  ),
                 );
               },
-              // onEndReached: () {
-              //   setState(() {
-              //     _currentPage++;
-              //   });
-              //   // Load more data for the next page
-              //   // ...
-              // },
             )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        backgroundColor: _counter == 0 ? Colors.orange : getColor(_counter),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
